a5c5c8cb58d4832859b8e08f1295794368d74161
Nosmow <nosmow.dev@gmail.com>
2024-04-01 09:51:50 -0500

    first commit

---
+++ cuantasVecesCabe/Operacion.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cuantasVecesCabe
+{
+    internal class Operacion
+    {
+        private int num1 = 0;
+        private int num2 = 0;
+        private int resultado = 0;
+
+        public void setNum1 ()
+        {
+            try
+            {
+                do
+                {
+                    Console.Write("\nNumero 1: ");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+
+                    if (num1 < 1000)
+                    {
+                        Console.WriteLine("\nNúmero incorrecto vuelva a intentar");
+                    }
+
+                } while (num1 < 1000);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void setNum2 ()
+        {
+            try
+            {
+                do
+                {
+                    Console.Write("\nNumero 2: ");
+                    num2 = Convert.ToInt32(Console.ReadLine());
+
+                    if (num2 > 100)
+                    {
+                        Console.WriteLine("\nNúmero incorrecto vuelva a intentar");
+                    }
+
+                } while (num2 > 100);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public int Calculo() 
+        {
+            return resultado = num1 / num2;
+        }
+    }
+}
---
+++ cuantasVecesCabe/Program.cs
@@ -0,0 +1,16 @@
+﻿using cuantasVecesCabe;
+
+try
+{
+    Operacion op = new Operacion();
+
+    Console.WriteLine("Por favor ingrese el primer número mayor a 1000 y el segundo menor a 100");
+
+    op.setNum1();
+    op.setNum2();
+    Console.WriteLine("\nCabe: " + op.Calculo() + " veces");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
